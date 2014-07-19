fe7f40a67ef706afdfe30c57ac140550a962c4d1
bujue <tougusheng@gmail.com>
2014-07-19 18:02:51 +0800

    Add SSL

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,19 +4,19 @@ class UsersController < ApplicationController
   end
 
   def create
-   @user = User.new(params[:user])
+   @user = User.new(user_params)
    if @user.save
-   #   flash[:success] ="welcome to the Sample App"
-    #  redirect_to @user
+     flash[:success] ="welcome to the Sample App"
+     redirect_to @user
    else
      render 'new'
     end
   end
 
- # def user_params
-  #  params.require(:user).permit(:name, :email, :password,
-  #                               :password_confirmation)
- # end
+  def user_params
+   params.require(:user).permit(:name, :email, :password,
+                               :password_confirmation)
+  end
 
   def show
     @user=User.find(params[:id])
